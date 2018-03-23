43552b74ce5efe0d814650aa3730c0a7cdcd731f
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-03-23 00:11:55 -0400

    Add toast message if userword and anotonym do not match

--- app/src/main/java/com/example/chady/assignment42/Enter_Values.java
+++ app/src/main/java/com/example/chady/assignment42/Enter_Values.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by Chady on 3/21/2018.
@@ -27,6 +29,22 @@ public class Enter_Values extends Activity {
         {
             //Intent j = new Intent(Enter_Values.this, MainActivity.class);
             //startActivity(j);
+
+            // pulls in text from keyboard in text area
+            EditText userWord = (EditText)findViewById(R.id.TFuserWord);
+            EditText userWordMatch = (EditText)findViewById(R.id.TFuserWordMatch);
+
+            // Converts  input from text field to a string
+            String userWordstr = userWord.getText().toString();
+            String userWordMatchstr = userWordMatch.getText().toString();
+
+            //checks if two words are a match
+            if(!userWordstr.equals(userWordMatchstr))
+            {
+                Toast wordNotFound = Toast.makeText(Enter_Values.this, "The user word and the antonym do not match !" , Toast.LENGTH_SHORT);
+                wordNotFound.show();
+            }
+
         }
     }
 
